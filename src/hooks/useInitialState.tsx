fefd6e2e53b689eeff3eb19d964c1672f2ad3c4b
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-09-29 15:54:37 -0400

    fix useInitialState

--- src/hooks/useInitialState.tsx
+++ src/hooks/useInitialState.tsx
@@ -4,29 +4,26 @@ import {
   allStateSelector,
   updateInitialLoad,
 } from "../redux-modules/settingsSlice";
-import { createServerApiHelpers, getServerApi } from "../backend/utils";
 import { useEffect } from "react";
 import { AppDispatch } from "../redux-modules/store";
+import { getSettings } from "../backend/utils";
 
 export const useIsInitiallyLoading = () => useSelector(initialLoadSelector);
 
 export const useSettingsState = () => useSelector(allStateSelector);
 
 export const useFetchInitialStateEffect = () => {
-  const serverApi = getServerApi();
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    if (serverApi) {
-      const { getSettings } = createServerApiHelpers(serverApi);
+    getSettings().then((result: any) => {
+      console.log(result);
 
-      getSettings().then((result) => {
-        if (result.success) {
-          const results = result.result || {};
-          //@ts-ignore
-          dispatch(updateInitialLoad(results));
-        }
-      });
-    }
+      if (result.success) {
+        const results = result.result || {};
+        //@ts-ignore
+        dispatch(updateInitialLoad(results));
+      }
+    });
   }, []);
 };
