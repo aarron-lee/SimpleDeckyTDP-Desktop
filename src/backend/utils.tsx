cc95b5e468dc8155466a13df17eb16d1c46c2ee7
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-05-09 10:26:06 -0400

    add SteamDeck enums

--- src/backend/utils.tsx
+++ src/backend/utils.tsx
@@ -10,6 +10,8 @@ export enum Devices {
   GPD_WIN4 = "G1618-04",
   ASUS_FLOW_Z13 = "ROG Flow Z13 GZ302EA_GZ302EA",
   ASUS_FLOW_Z13_SHORT = "ROG Flow Z13 GZ302",
+  STEAM_DECK_LCD = "Jupiter",
+  STEAM_DECK_OLED = "Galileo",
 }
 
 export enum AdvancedOptionsEnum {
@@ -43,6 +45,10 @@ export enum RogAllyAdvancedOptions {
 
 export enum LegionGoAdvancedOptions {
   CUSTOM_TDP_MODE = "lenovoCustomTdpMode",
+}
+
+export enum SteamDeckAdvancedOptions {
+  DECK_CUSTOM_TDP_LIMITS = "deckCustomTdpLimits",
 }
 
 export const DesktopAdvancedOptions = [
