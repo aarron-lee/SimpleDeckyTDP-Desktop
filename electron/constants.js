4fcff5d39d3d047a51dd475847b47cac412b631e
Aarron Lee <aarron-lee@users.noreply.github.com>
2026-04-22 09:00:21 -0500

    add constants.js

---
+++ electron/constants.js
@@ -0,0 +1,7 @@
+const IPC_ACTIONS = {
+  GAMEPAD_BUTTON_PRESS: "gamepadButtonPress",
+  GET_MIN_ON_CLOSE: "getMinOnCloseSetting",
+  SET_MIN_ON_CLOSE: "setMinOnCloseSetting",
+};
+
+module.exports = { IPC_ACTIONS };
